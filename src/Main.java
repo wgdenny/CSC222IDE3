7ca38d823ba318111841715e5d515e93aaf205f2
Will Denny <sparky.wgd@gmail.com>
2023-04-26 16:50:42 -0400

    Have added the sets in stead of hard coding

--- src/Main.java
+++ src/Main.java
@@ -30,9 +30,15 @@ class Main {
             double salary = input.nextDouble();
             System.out.println("Where are you located?");
             int terr = input.nextInt();
-            System.out.println("You are located in " + terr);
-            //hybEmploy.setId(idNum);
-            //hybEmploy.setSalary(salary);
+            EmployeeWithTerritory Emp2 = new EmployeeWithTerritory();
+            Emp2.setId(idNum);
+            Emp2.setSalary(salary);
+            Emp2.setTerritory(terr);
+            System.out.println("Your employee id is " + Emp2.getId());
+            System.out.println("Your salary in $" + Emp2.getSalary());
+            System.out.println("You are located in " + Emp2.getTerritory());
+
+
         }
 
         if(employType == 3){
@@ -50,14 +56,15 @@ class Main {
             int wifi = input.nextInt();
             HybridEmployee hybEmploy = new HybridEmployee();
             hybEmploy.setId(idNum);
-            hybEmploy.setSalary(salary);
             hybEmploy.setDaysAtWork(daWork);
-            hybEmploy.displayHybridSalary();
+            hybEmploy.setDaysAtHome(daHome);
+            hybEmploy.setWifiSpeed(wifi);
+            hybEmploy.setSalaryPerHour(hrSal);
             System.out.println("You have chosen the Hybrid employee!");
-            System.out.println("You will have " + hybEmploy.getDaysAtHome() + " at work.");
-            System.out.println("You will have " + daHome + " at home working.");
-            System.out.println("Your hourly pay will be $"+ hrSal);
-            System.out.println("You have " + wifi + " megabits from home which qualifies you to work from home.");
+            System.out.println("You will have " + hybEmploy.getDaysAtWork() + " days at work.");
+            System.out.println("You will have " + hybEmploy.getDaysAtHome() + " days at home working.");
+            System.out.println("Your hourly pay will be "+ hybEmploy.getSalaryPerHour());
+            System.out.println("You have " + hybEmploy.getWifiSpeed() + " megabits from home which qualifies you to work from home.");
         }
 
 
